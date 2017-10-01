d42f182aac94ae376ba9694187a3fe8e08ec653d
徐梓琅 <272251416@qq.com>
2017-10-01 20:04:54 +0800

    update enposrt

--- 3.爬取中英文网站/zn-enPost.py
+++ 3.爬取中英文网站/zn-enPost.py
@@ -1,4 +1,4 @@
-#-*-coding:utf-8-*-
+#-- coding:utf-8 --
 import urllib
 import nltk
 from bs4 import BeautifulSoup
@@ -12,7 +12,7 @@ zg_text = soup.select(".qh_zg")
 zg_Post = ''
 #print (zg_text) 
 en_text = soup.select(".qh_en")
-en_Post = ''
+en_Post = ' '
 #print (en_text)
 for i in zg_text:
 	#print (i.encode('utf-8'))
@@ -22,9 +22,10 @@ for i in zg_text:
 	#a += 1
 	zg_Post += i.get_text()
 for m in en_text: 
-	print (m.get_text())
-	en_Post += m.get_text()
+	print (type(m.get_text()))
+	en_Post += str(m.get_text())
 print zg_Post
+print type(en_Post)
 print en_Post
 #text = soup.get_text(strip=True)
 #print (text)
