f0cba23be70a8460f0e9156046dfef253ad54c4d
徐梓琅 <272251416@qq.com>
2017-10-05 15:55:06 +0800

    something changed

--- 3.爬取中英文网站/zn-enPost.py
+++ 3.爬取中英文网站/zn-enPost.py
@@ -26,10 +26,6 @@ for m in en_text:
 print zn_Post
 print en_Post
 
-En_Post = en_Post.decode("utf-8")
-filtrate = re.compile(u'[^\u4E00-\u9FA5]')#非中文
-nEn_Post = filtrate.sub(r'', En_Post)#replace
-print nEn_Post.encode('utf-8')
 
 conn = pymysql.connect(host="127.0.0.1", user='root', passwd='root', db='zn-enpost')
 cur = conn.cursor()
