213297582a205ead77584ed00dfaeb66763d9ca3
徐梓琅 <272251416@qq.com>
2017-10-05 21:45:06 +0800

    nobody likes me nobody likes my tshit.

--- 3.爬取中英文网站/zn-enPost.py
+++ 3.爬取中英文网站/zn-enPost.py
@@ -9,7 +9,7 @@ response = urllib.urlopen('http://www.kekenet.com/read/201709/526460.shtml')
 html = response.read()
 soup = BeautifulSoup(html, "lxml")
 
-postTitle = soup.select("#nrtitle")[0].get_text().encode('utf-8')
+postTitle = soup.select("#nrtitle")[0].get_text().decode('gbk').encode('utf-8')
 print postTitle
 
 zn_text = soup.select(".qh_zg")
@@ -19,16 +19,20 @@ en_text = soup.select(".qh_en")
 en_Post = ' '
 
 for i in zn_text:
-	zn_Post += i.get_text().encode('utf-8')
+	zn_Post += i.get_text()
 for m in en_text: 
-	en_Post += m.get_text().encode('utf-8')
+	en_Post += m.get_text()
 
+print type(zn_Post)
+print type(en_Post)
 print zn_Post
 print en_Post
 
+zn_Post = zn_Post.decode('gbk').encode('utf-8')
+en_Post = en_Post.decode('gbk').encode('utf-8')
 
-conn = pymysql.connect(host="127.0.0.1", user='root', passwd='root', db='zn-enpost')
+conn = pymysql.connect(host="127.0.0.1", user='root', passwd='root', db='zn-enpost',charset="utf8")
 cur = conn.cursor()
-cur.execute('insert ignore into znEnPost (postTitle, Entext, ZnText) values (%s, %s, %s)', [postTitle, en_Post, zn_Post])
+cur.execute('insert ignore into znEnPost (postTitle, EnText, ZnText) values (%s, %s, %s)', [postTitle, en_Post, zn_Post])
 conn.commit()
 cur.close()
