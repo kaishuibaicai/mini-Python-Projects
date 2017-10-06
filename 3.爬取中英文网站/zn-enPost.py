6e8e0c1c0bb0256c4b3781336b175ac42b9afe5c
徐梓琅 <272251416@qq.com>
2017-10-06 21:45:31 +0800

    smc

--- 3.爬取中英文网站/zn-enPost.py
+++ 3.爬取中英文网站/zn-enPost.py
@@ -5,16 +5,18 @@ from bs4 import BeautifulSoup
 import pymysql
 import time
 
-base = 'http://www.kekenet.com'
-task = '/read/201710/526908.shtml'
 
+base = 'http://www.kekenet.com'
+task = '/read/201709/526085.shtml'
+n = 1
 def spider(url):
+	global n
 	response = urllib.urlopen(base+task)
 	html = response.read()
 	soup = BeautifulSoup(html, "lxml")
 
 	postTitle = soup.select("#nrtitle")[0].get_text().decode('gbk', 'ignore').encode('utf-8')
-	print '[数据获取成功:]  '+postTitle
+	print '[第%d篇文章数据获取成功:]  %s' % (n, postTitle)
 
 	zn_text = soup.select(".qh_zg")
 	zn_Post = ''
@@ -33,6 +35,7 @@ def spider(url):
 	en_Post = en_Post[:-25].decode('gbk', 'ignore').encode('utf-8')
 
 	result = [postTitle, en_Post, zn_Post, newtask]
+	n += 1
 
 	return result
 
@@ -47,31 +50,7 @@ try:
 		cur.execute('insert ignore into znenpost (postTitle, EnText, ZnText) values (%s, %s, %s)', [Result[0], Result[1], Result[2]])
 		print ('已经保存到数据库')
 		task = Result[3]
-		time.sleep(0.5)
+		#time.sleep(0.5)
 finally:
 	conn.commit()
 	cur.close()
-##############################################################################
-# response = urllib.urlopen(base+task)
-# html = response.read()
-# soup = BeautifulSoup(html, "lxml")
-
-# postTitle = soup.select("#nrtitle")[0].get_text().decode('gbk').encode('utf-8')
-# print '[爬取成功:]  '+postTitle
-
-# zn_text = soup.select(".qh_zg")
-# zn_Post = ''
-
-# en_text = soup.select(".qh_en")
-# en_Post = ' '
-
-# for i in zn_text:
-# 	zn_Post += i.get_text()
-# for m in en_text: 
-# 	en_Post += m.get_text()
-
-
-# zn_Post = zn_Post.decode('gbk').encode('utf-8')
-# en_Post = en_Post[:-25].decode('gbk').encode('utf-8')
-
-
