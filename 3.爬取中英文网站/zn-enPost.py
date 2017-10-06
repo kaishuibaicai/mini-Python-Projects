5e5a6b0195c5bd874479bda9a03e251dd631ebff
徐梓琅 <272251416@qq.com>
2017-10-06 17:50:46 +0800

    zhangliqin

--- 3.爬取中英文网站/zn-enPost.py
+++ 3.爬取中英文网站/zn-enPost.py
@@ -3,17 +3,18 @@ import urllib
 import re
 from bs4 import BeautifulSoup
 import pymysql
+import time
 
 base = 'http://www.kekenet.com'
-task = '/read/201710/526886.shtml'
+task = '/read/201710/526908.shtml'
 
 def spider(url):
 	response = urllib.urlopen(base+task)
 	html = response.read()
 	soup = BeautifulSoup(html, "lxml")
 
-	postTitle = soup.select("#nrtitle")[0].get_text().decode('gbk').encode('utf-8')
-	print '[爬取成功:]  '+postTitle
+	postTitle = soup.select("#nrtitle")[0].get_text().decode('gbk', 'ignore').encode('utf-8')
+	print '[数据获取成功:]  '+postTitle
 
 	zn_text = soup.select(".qh_zg")
 	zn_Post = ''
@@ -26,10 +27,30 @@ def spider(url):
 	for m in en_text: 
 		en_Post += m.get_text()
 
-	zn_Post = zn_Post.decode('gbk').encode('utf-8')
-	en_Post = en_Post[:-25].decode('gbk').encode('utf-8')
-	result = [postTitle, en_Post, zn_Post]
+	newtask = soup.select(".fl")[0].a['href']
+
+	zn_Post = zn_Post.decode('gbk', 'ignore').encode('utf-8')
+	en_Post = en_Post[:-25].decode('gbk', 'ignore').encode('utf-8')
+
+	result = [postTitle, en_Post, zn_Post, newtask]
+
 	return result
+
+
+conn = pymysql.connect(host="127.0.0.1", user='root', passwd='root', db='zn-enpost', charset='utf8')
+cur = conn.cursor()
+
+
+try:
+	while 1:
+		Result = spider(base+task)
+		cur.execute('insert ignore into znenpost (postTitle, EnText, ZnText) values (%s, %s, %s)', [Result[0], Result[1], Result[2]])
+		print ('已经保存到数据库')
+		task = Result[3]
+		time.sleep(0.5)
+finally:
+	conn.commit()
+	cur.close()
 ##############################################################################
 # response = urllib.urlopen(base+task)
 # html = response.read()
@@ -54,10 +75,3 @@ def spider(url):
 # en_Post = en_Post[:-25].decode('gbk').encode('utf-8')
 
 
-Result = spider(base+task)
-
-conn = pymysql.connect(host="127.0.0.1", user='root', passwd='root', db='zn-enpost',charset='utf8')
-cur = conn.cursor()
-cur.execute('insert ignore into znenpost (postTitle, EnText, ZnText) values (%s, %s, %s)', [Result[0], Result[1], Result[2]])
-conn.commit()
-cur.close()
