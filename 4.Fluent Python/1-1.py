22c5ebaeeb2fc19ea6e03e6ce6e30e9264a20435
徐梓琅 <272251416@qq.com>
2017-10-09 15:18:54 +0800

    update

--- 4.Fluent Python/1-1.py
+++ 4.Fluent Python/1-1.py
@@ -3,7 +3,7 @@ import collections
 Card = collections.namedtuple('card', ['rank', 'suit'])
 
 class FrenchDeck:
-	ranks = [str(n) for n range(2, 11)] + list('JQKA')
+	ranks = [str(n) for n in range(2, 11)] + list('JQKA')
 	suits = 'spades diamonds clubs hearts'.split()
 
 	def __init__(self):
