58fb0076b8d05fac5a3f84c84050791febb1d3a6
徐梓琅 <272251416@qq.com>
2017-10-13 18:58:30 +0800

    sc

--- 4.Fluent Python/deque-test.py
+++ 4.Fluent Python/deque-test.py
@@ -9,5 +9,5 @@ fancy_loading = deque('>--------------------')
 while 1:
 	print '\r%s' % ''.join(fancy_loading)
 	fancy_loading.rotate(1)
-	sys.stdout.flush()
+	#sys.stdout.flush()
 	time.sleep(0.08)
