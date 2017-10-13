9e81c19ed17181f99439659b8af08b29790fb727
徐梓琅 <272251416@qq.com>
2017-10-13 13:57:50 +0800

    add deque

---
+++ 4.Fluent Python/deque-test.py
@@ -0,0 +1,13 @@
+#-*- coding: utf-8 -*-
+
+import sys
+import time
+from collections import deque
+
+fancy_loading = deque('>--------------------')
+
+while 1:
+	print '\r%s' % ''.join(fancy_loading)
+	fancy_loading.rotate(1)
+	sys.stdout.flush()
+	time.sleep(0.08)
