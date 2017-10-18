31746d4ed651cbe005ec83166361cf8886d7a818
徐梓琅 <272251416@qq.com>
2017-10-18 16:23:50 +0800

    update something

--- 6.numpyL/creatDataFrame.py
+++ 6.numpyL/creatDataFrame.py
@@ -1,3 +1,17 @@
 import pandas as pd
+s1 = pd.Series([1, 2])
+print s1
+s2 = pd.Series(['Boris', 'Mikhail'])
+print s2
 
 print pd.DataFrame([s1, s2])
+
+new = pd.DataFrame(
+	[
+		[1, 2],
+		['Boris', 'Mikhail']
+	],
+	columns=['columns1', 'columns2'],
+	index=['row1', 'row2']
+	)
+print new
