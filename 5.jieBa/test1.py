e0ea523ac83245a2c7907a1044865b8b527d24ed
徐梓琅 <272251416@qq.com>
2017-10-18 14:44:21 +0800

    add 6

--- 5.jieBa/test1.py
+++ 5.jieBa/test1.py
@@ -3,7 +3,7 @@ import jieba
 import jieba.posseg as pseg
 
 seg_list = jieba.cut('他来到了厦门软件园', cut_all=True)
-words = pseg.cut('我爱北京天安')
+words = pseg.cut('小明硕士毕业于中国科学院计算所，后在日本京都大学深造')
 print ('Full Mode:' + '/ '.join(seg_list))
 for word, flag in words:
 	print ('%s %s' % (word, flag))
