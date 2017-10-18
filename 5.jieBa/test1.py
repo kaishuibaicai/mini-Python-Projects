c00bd306d4e916c1344de8981198676ccc0fd993
徐梓琅 <272251416@qq.com>
2017-10-18 10:15:26 +0800

    add pseg

--- 5.jieBa/test1.py
+++ 5.jieBa/test1.py
@@ -1,6 +1,9 @@
 #encoding=utf-8
 import jieba
+import jieba.posseg as pseg
 
 seg_list = jieba.cut('他来到了厦门软件园', cut_all=True)
-
+words = pseg.cut('我爱北京天安')
 print ('Full Mode:' + '/ '.join(seg_list))
+for word, flag in words:
+	print ('%s %s' % (word, flag))
