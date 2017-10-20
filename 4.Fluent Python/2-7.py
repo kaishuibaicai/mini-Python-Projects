5f30756e99a612c50f490a6afcf1588b7bd9b17d
徐梓琅 <272251416@qq.com>
2017-10-20 09:44:18 +0800

    sc

--- 4.Fluent Python/2-7.py
+++ 4.Fluent Python/2-7.py
@@ -6,7 +6,8 @@ for passport in sorted(traveler_ids):
 '''sort 与 sorted 区别：
 sort 是应用在 list 上的方法，sorted 可以对所有可迭代的对象进行排序操作。
 list 的 sort 方法返回的是对已经存在的列表进行操作，而内建函数 sorted 方法返回的是一个新的 list，
-而不是在原来的基础上进行的操作。'''
+而不是在原来的基础上进行的操作。
+参考：http://www.runoob.com/python/python-func-sorted.html'''
 
 for country, _ in traveler_ids:
 	print (country)
