b7b527f7fcd294eb88f5c769465b618f51561b2a
徐梓琅 <272251416@qq.com>
2017-10-22 11:26:27 +0800

    sc

--- 7.股票名称/search/search.py
+++ 7.股票名称/search/search.py
@@ -21,9 +21,10 @@ try:
 		print name
 		keyword = name + "\"环境报告书\""
 		url = "https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=0&rsv_idx=1&tn=baidu&wd=" + keyword + "&rsv_pq=8b571d160000138b&rsv_t=05685piESqzn2F8YfpupAfHH%2Fjhsv4%2BV61GM%2Fit7CV3Waq%2FBfnd00QohnZM&rqlang=cn&rsv_enter=0&rsv_sug3=13&inputT=329&rsv_sug4=330"
+		print url
 		response = urllib.urlopen(url)
 		html = response.read()
-		soup = BeautifulSoup(html, "html")
+		soup = BeautifulSoup(html)
 		print soup
 		# r = soup.select(".tip_head")
 		# if r:
