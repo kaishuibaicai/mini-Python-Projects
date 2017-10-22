115000e8c13c2316023c103aa48d997d300e292b
徐梓琅 <272251416@qq.com>
2017-10-22 11:18:11 +0800

    sc

--- 7.股票名称/search/search.py
+++ 7.股票名称/search/search.py
@@ -19,12 +19,17 @@ try:
 	for name in names:
 		name = "\"{}\"".format(name.strip())
 		print name
-		keyword = (name + "\"环境报告书\"")
+		keyword = name + "\"环境报告书\""
 		url = "https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=0&rsv_idx=1&tn=baidu&wd=" + keyword + "&rsv_pq=8b571d160000138b&rsv_t=05685piESqzn2F8YfpupAfHH%2Fjhsv4%2BV61GM%2Fit7CV3Waq%2FBfnd00QohnZM&rqlang=cn&rsv_enter=0&rsv_sug3=13&inputT=329&rsv_sug4=330"
 		response = urllib.urlopen(url)
 		html = response.read()
-		soup = BeautifulSoup(html, "lxml")
-	
+		soup = BeautifulSoup(html, "html")
+		print soup
+		# r = soup.select(".tip_head")
+		# if r:
+		# 	print "没有结果"
+		# else:
+		# 	print "有结果"
 finally:
 	if f:
 		f.close()	
