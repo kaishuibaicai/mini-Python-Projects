d8d53003216841b5fa1e977c3cb9ce07fc9120f8
徐梓琅 <272251416@qq.com>
2017-10-24 21:40:09 +0800

    有志者事竟成，苦心人天不负！

--- 7.股票名称/search/search.py
+++ 7.股票名称/search/search.py
@@ -1,17 +1,7 @@
 #-- coding:utf-8 --
 
-# from selenium import webdriver
-
-# driver = webdriver.Chrome()
-# driver.get("https://www.baidu.com")
-# name = "\"天宝食品\""
-# keyword = name + "\"环境报告书\""
-# driver.find_element_by_id("kw").send_keys(keyword.decode('utf-8'))
-# driver.find_element_by_id("su").click()
 import re
-import urllib
 import requests
-from bs4 import BeautifulSoup
 from pyquery import PyQuery as Pq
 
 class BaiduSearchSpider(object):
@@ -63,13 +53,6 @@ try:
 		name = "\"{}\"".format(name.strip())
 		print name
 		keyword = name + "\"环境报告书\""
-		# url = 'http://www.baidu.com'
-		# #url = "https://www.baidu.com/s?ie=utf-8&f=8&rsv_bp=0&rsv_idx=1&tn=baidu&wd=" + keyword + "&rsv_pq=8b571d160000138b&rsv_t=05685piESqzn2F8YfpupAfHH%2Fjhsv4%2BV61GM%2Fit7CV3Waq%2FBfnd00QohnZM&rqlang=cn&rsv_enter=0&rsv_sug3=13&inputT=329&rsv_sug4=330"
-		# print url
-		# response = urllib.urlopen(url)
-		# html = response.read()
-		# soup = BeautifulSoup(html, 'html.parser')
-		# print soup
 		bdsearch = BaiduSearchSpider(keyword) 
 		originalurls = bdsearch.originalURLs
 		print '=======Original URLs========'
