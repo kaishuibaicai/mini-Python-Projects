c112e9e5dca8704f48c18b37e8a397768670539c
徐梓琅 <272251416@qq.com>
2017-11-06 17:54:12 +0800

    sc

--- 9.标注角色计数/counter.py
+++ 9.标注角色计数/counter.py
@@ -4,7 +4,7 @@ import xml.etree.ElementTree as ET
 from bs4 import BeautifulSoup
 import urllib
 
-path = "C:\Users\Administrator\Desktop\huoying-zl"
+path = "C:\Users\Administrator\Desktop\kenan-zl"
 dirs = os.listdir(path)
 nameSets = []
 name_counts = {}
