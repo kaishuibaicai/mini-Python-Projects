c274ea409add39ce745e1947df02ad3e6a2eb094
徐梓琅 <272251416@qq.com>
2017-11-06 16:58:48 +0800

    sc

--- 9.标注角色计数/README.md
+++ 9.标注角色计数/README.md
@@ -1,3 +1,4 @@
 ###参考知识
 1.[Python读取文件夹下的所有文件](http://blog.csdn.net/lzgs_4/article/details/50371030)
 2.[深入解读Python解析XML的几种方式](http://codingpy.com/article/parsing-xml-using-python/)
+3.[Python语言中计数方法的演变](http://codingpy.com/article/counting-things-in-python/)
--- 9.标注角色计数/xmlPtest.py
+++ 9.标注角色计数/xmlPtest.py
@@ -1,9 +1,21 @@
-# coding:utf-8
+#-- coding:utf-8 --
 import xml.etree.ElementTree as ET
+from bs4 import BeautifulSoup
+import urllib
 
-tree = ET.ElementTree(file='C:/Users/Administrator/Desktop/huoying-zl/mgdm_characterDetectionAI_Origin_Video_20171019_00000053/mgdm_characterDetectionAI_Origin_Video_20171019_00000053_Preprocessing_00000917_00.xml')
-# root = tree.getroot()
-# print root.tag
-# print root.attrib
-for elem in tree.iter(tag='name'):
-	print elem.tag, elem.attrib
+nameSets = []
+response = urllib.urlopen('file:///C:/Users/Administrator/Desktop/huoying-zl/mgdm_characterDetectionAI_Origin_Video_20171019_00000053/mgdm_characterDetectionAI_Origin_Video_20171019_00000053_Preprocessing_00000917_00.xml')
+html = response.read()
+soup = BeautifulSoup(html, "xml")
+names = soup.select("name")
+
+for name in names:
+	nameSets.append(name.get_text())
+print nameSets
+
+# tree = ET.ElementTree(file='C:/Users/Administrator/Desktop/huoying-zl/mgdm_characterDetectionAI_Origin_Video_20171019_00000053/mgdm_characterDetectionAI_Origin_Video_20171019_00000053_Preprocessing_00000917_00.xml')
+# # root = tree.getroot()
+# # print root.tag
+# # print root.attrib
+# for elem in tree.iter(tag='name'):
+# 	print elem.tag, elem.attrib
