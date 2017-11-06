4ec1091f431914763579678ee318de43b1fad1a3
徐梓琅 <272251416@qq.com>
2017-11-06 17:48:27 +0800

    sc

--- 9.标注角色计数/counter.py
+++ 9.标注角色计数/counter.py
@@ -6,14 +6,28 @@ import urllib
 
 path = "C:\Users\Administrator\Desktop\huoying-zl"
 dirs = os.listdir(path)
+nameSets = []
 name_counts = {}
 
 for Dir in dirs:
 	if not os.path.isdir(Dir):
 		files = os.listdir(path + '/' + Dir)
 		for file in files:
-			Path = 'file://' + path + '/' + Dir + '/' + file
-			response = urllib.urlopen('file:///C:/Users/Administrator/Desktop/huoying-zl/mgdm_characterDetectionAI_Origin_Video_20171019_00000053/mgdm_characterDetectionAI_Origin_Video_20171019_00000053_Preprocessing_00000917_00.xml')
-html = response.read()
-soup = BeautifulSoup(html, "xml")
-names = soup.select("name")
+			Path = 'file:///' + path + '/' + Dir + '/' + file
+			print Path
+			response = urllib.urlopen(Path)
+			html = response.read()
+			soup = BeautifulSoup(html, "xml")
+			names = soup.select("name")
+
+			for name in names:
+				nameSets.append(name.get_text())
+			print 'Done'
+
+for n in nameSets:
+    if name_counts.has_key(n):
+        name_counts[n] = name_counts[n] + 1
+    else:
+        name_counts[n] = 1
+for item in name_counts:
+	print item, name_counts[item]
