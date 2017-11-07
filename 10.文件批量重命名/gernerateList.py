22e12e2ebe35be18945a759c8f97e3eef906a2d2
徐梓琅 <272251416@qq.com>
2017-11-07 13:32:08 +0800

    sc

--- 10.文件批量重命名/gernerateList.py
+++ 10.文件批量重命名/gernerateList.py
@@ -3,3 +3,19 @@ import os
 
 path = "C:\Users\Administrator\Desktop\missions\huoying\Preprocessing\Preprocessing\Image\\new"
 dirs = os.listdir(path)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+with open("huoyingthename.txt", 'a+') as s:
+			s.write(name + '\n')
