573ce7f95d27364de704232967d57a10b1e70d6f
徐梓琅 <272251416@qq.com>
2017-11-07 13:31:44 +0800

    sc

--- 10.文件批量重命名/rename.py
+++ 10.文件批量重命名/rename.py
@@ -4,11 +4,16 @@ import os
 path = "C:\Users\Administrator\Desktop\missions\huoying\Preprocessing\Preprocessing\Image\\new"
 dirs = os.listdir(path)
 
-path = 'D:\\图片\\'
-for file in os.listdir(path):
-    if os.path.isfile(os.path.join(path,file))==True:
-        if file.find('.')<0:
-            newname=file+'rsfdjndk.jpg'
-            os.rename(os.path.join(path,file),os.path.join(path,newname))
-            print file,'ok'
-#        print file.split('.')[-1] 
+for dir in dirs:
+	for file in os.listdir(path + '\\' + dir):
+	    if os.path.isfile(os.path.join(path,dir)):
+	        if file.find('.')<0:
+	            newname=file+'rsfdjndk.jpg'
+	            os.rename(os.path.join(path,file),os.path.join(path,newname))
+	            print file,'ok'
+	#        print file.split('.')[-1] 
+
+
+
+	with open("huoyingthename.txt", 'a+') as s:
+			s.write(name + '\n')
