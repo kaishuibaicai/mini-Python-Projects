65741c299e1e3388abe6154fc36cf90db37c417d
徐梓琅 <272251416@qq.com>
2017-11-07 10:27:47 +0800

    sc

--- 10.文件批量重命名/test.py
+++ 10.文件批量重命名/test.py
@@ -1,4 +1,6 @@
-for month in range(1,13):
-    if month < 10 :
-        month = '0{}'.format(month)
-    print month
+# for month in range(1,13):
+#     if month < 10 :
+#         month = '0{}'.format(month)
+#     print month
+a = 596
+print '%08d' % a
