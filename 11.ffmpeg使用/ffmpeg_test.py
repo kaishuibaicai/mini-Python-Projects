9dc12c1eb31b76fc8552f835298a5cc0587770c0
徐梓琅 <272251416@qq.com>
2017-11-07 13:08:58 +0800

    sc

--- 11.ffmpeg使用/ffmpeg_test.py
+++ 11.ffmpeg使用/ffmpeg_test.py
@@ -1,9 +1,9 @@
  import os, subprocess
 # 抽帧
 def cut_one_piece():
-    path = 'F:\\AI\\预处理视频\\海贼王\\Theater version'
+    path = 'C:\Users\Administrator\Desktop\missions\huoying\Preprocessing\Preprocessing\Image\\new'
 
-    output_file_path = 'F:/AI/preprocessing/one_piece/mgdm_characterDetectionAI_Origin_Video_20171030'
+    output_file_path = 'C:\Users\Administrator\Desktop\missions\huoying\Preprocessing\Preprocessing\Image\\new\mgdm_characterDetectionAI_Origin_Video_20171106_00000067'
     # files = os.listdir(path)
 
     # file_name = 'mgdm_mgAI_Origin_Video_1506689951_00000001.mp4'
@@ -15,6 +15,6 @@ def cut_one_piece():
         os.mkdir(output_path)
         os.chdir(output_path)
 
-        output_file_name = file[:-4] + '_00_Preprocessing_20171030_%08d_00.jpg'
+        output_file_name = file[:-4] + '_00_Preprocessing_20171106_%08d_00.jpg'
 
         subprocess.call(['ffmpeg', '-i', os.path.join(path, file), '-r', '1', output_file_name])    # 抽帧命令
