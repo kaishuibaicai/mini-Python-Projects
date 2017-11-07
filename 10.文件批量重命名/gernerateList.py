6e2bd3f93602bd3fbec854d059f145cad019a3ba
徐梓琅 <272251416@qq.com>
2017-11-07 14:01:19 +0800

    sc

--- 10.文件批量重命名/gernerateList.py
+++ 10.文件批量重命名/gernerateList.py
@@ -6,10 +6,8 @@ dirs = os.listdir(path)
 
 for dir in dirs:
 	print dir
-	for file in os.listdir(path + '/' + dir):
-		print file
-	    if os.path.isfile(os.path.join(path,dir)):
-	        if file.find('.mp4'):
-	            with open("C:\Users\Administrator\Desktop\missions\huoying\Preprocessing\Preprocessing\Image\\new\huoyingthename.txt", 'a+') as s:
-					s.write(file + "    " + dir + '\n')
-	            	print file,'ok'
+	for file in os.listdir(path + '\\' + dir):
+	    if file.endswith('.mp4'):
+	    	print file
+	        with open("C:\Users\Administrator\Desktop\missions\huoying\Preprocessing\Preprocessing\Image\huoyingthename.txt", 'a+') as s:
+				s.write(file + "    " + dir + '\n')
