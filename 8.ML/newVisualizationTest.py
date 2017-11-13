5faf3b8de96b7c6f90f1225914527c2c4aa5ae12
徐梓琅 <272251416@qq.com>
2017-11-13 18:12:02 +0800

    sc

---
+++ 8.ML/newVisualizationTest.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+
+ts = pd.Series(np.random.randn(1000), index=pd.date_range('1/1/2000', periods=1000))
+ts = ts.cumsum()
+ts.plot()
+
+# df = pd.DataFrame(np.random.randn(1000, 4), index=ts.index, columns=list('ABCD'))
+# df = df.cumsum()
+# plt.figure()
+# df.plot()
