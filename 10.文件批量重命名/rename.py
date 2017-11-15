bd80883a5c9952b4d5ab7892f0969ea7b630a594
徐梓琅 <272251416@qq.com>
2017-11-15 17:11:16 +0800

    sc

--- 10.文件批量重命名/rename.py
+++ 10.文件批量重命名/rename.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import os
 
-path = "C:\Users\Administrator\Desktop\cosplay-筛选"
+path = "C:\Users\Administrator\Desktop\cosplay"
 dirs = os.listdir(path)
 
 for dir in dirs:
@@ -9,7 +9,8 @@ for dir in dirs:
 	n = 1
 	for file in os.listdir(path + '\\' + dir):
 	    if file.endswith('.jpg'):
-	    	newname = dir + '_Preprocessing_%08d_00.jpg' % n
+	    	#newname = dir + '_Preprocessing_%08d_00.jpg' % n
+	    	newname = 'mgdm_relevantCharacterAI_Origin_Image_20171116_%08d_00_Preprocessing_20171116_00.jpg' % n
 	        os.rename(path + '\\' + dir + '\\' + file, path + '\\' + dir + '\\' + newname)
 	        n = n + 1
 	        print newname, 'Done'
