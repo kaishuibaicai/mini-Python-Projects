d101b6dc3bfd33082b9350aa2dfa082b7729d81f
徐梓琅 <272251416@qq.com>
2017-11-15 09:49:37 +0800

    sc

--- 11.ffmpeg使用/test.py
+++ 11.ffmpeg使用/test.py
@@ -2,7 +2,6 @@
 import os, subprocess
 # 抽帧, 输入：原视频文件夹，输出目录，输出重命名记录
 def cut_image(resource_path, output_file_path, output_record):
-    record = open(output_record, 'w')
 
     files = os.listdir(resource_path)
     index = 0
@@ -15,7 +14,8 @@ def cut_image(resource_path, output_file_path, output_record):
 
         pre_file_name = 'mgdm_characterDetectionAI_Origin_Video_20171115_%08d_00' % index
         output_path = os.path.join(output_file_path, pre_file_name)
-
+        with open("C:\Users\Administrator\Desktop\missions\huoying\Preprocessing\Preprocessing\Image\huoyingthename.txt", 'a+') as s:
+            s.write('%s\t%s\n' % (file, pre_file_name))
         record.write('%s\t%s\n' % (file, pre_file_name))
 
         os.mkdir(output_path)
@@ -24,7 +24,6 @@ def cut_image(resource_path, output_file_path, output_record):
         output_file_name = pre_file_name + '_Preprocessing_20171115_%08d_00.jpg'
 
         subprocess.call(['ffmpeg', '-i', file_path, '-r', '1', output_file_name])  # 抽帧命令
-    record.close()
  
 rp = 'E:\huoying-new2'
 op = 'E:\huoying-new2'
