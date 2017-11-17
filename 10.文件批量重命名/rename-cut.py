5fa233ce75c5ccab86add645f244b65c74899c74
徐梓琅 <272251416@qq.com>
2017-11-17 15:33:40 +0800

    sc

--- 10.文件批量重命名/rename-cut.py
+++ 10.文件批量重命名/rename-cut.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 import os
 
-path = "C:\Users\Administrator\Desktop\data\\20171027\\20171027\label_results"
+path = "C:\Users\Administrator\Desktop\data\\20171031\\20171031\\rich_text"
 
 
 for file in os.listdir(path):
-
+	if file.endswith('.xml'):
 	#newname = dir + '_Preprocessing_%08d_00.jpg' % n
-	newname = file[0:94] + '.xml'
-	print newname
-	os.rename(path + '\\' + file, path + '\\' + newname)
+		newname = file[0:94] + '.xml'
+		print newname
+		os.rename(path + '\\' + file, path + '\\' + newname)
 
-	print newname, 'Done'
+		print newname, 'Done'
