dd546f47c2e90a9a742758a88a96d481767cee39
徐梓琅 <272251416@qq.com>
2017-11-21 10:30:56 +0800

    better

--- 10.文件批量重命名/rename2.py
+++ 10.文件批量重命名/rename2.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 import os
 
-path = "C:\Users\Administrator\Desktop\cosplayData\cosplay-all"
+path = "C:\Users\Administrator\Desktop\missions\cosplay"
 dirs = os.listdir(path)
 n = 1
 for dir in dirs:
-	
+	with open("C:\Users\Administrator\Desktop\DirList.txt", 'a+') as s:
+				s.write(str(n) + "    " + dir + '\n')
 	for file in os.listdir(path + '\\' + dir):
 	    
 	    #newname = dir + '_Preprocessing_%08d_00.jpg' % n
-	    newname = file[0:84] + '_%02d' % n + '_label_20171117_00_00' + '.xml'
+	    #newname = file[0:84] + '_%02d' % n + '_label_20171117_00_00' + '.xml'
+	    newname = '%02d_' % n +  file
 	    os.rename(path + '\\' + dir + '\\' + file, path + '\\' + dir + '\\' + newname)
 	    
 	    print newname, 'Done'
