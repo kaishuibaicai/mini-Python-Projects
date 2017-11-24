56786c6d2278c52a1858ddc8e148ff5a835588f0
徐梓琅 <272251416@qq.com>
2017-11-24 11:32:57 +0800

    better

--- 10.文件批量重命名/findfile.py
+++ 10.文件批量重命名/findfile.py
@@ -2,6 +2,7 @@
 import os
 import shutil
 L = []
+R = []
 pathR = "C:\Users\Administrator\Desktop\cosplay1112\luotianyi"
 pathL = "C:\Users\Administrator\Desktop\cosplay1112\luotianyi"
 
@@ -11,11 +12,16 @@ for file in filesL:
 print L
 filesR = os.listdir(pathR)
 for file in filesR:
-	if file[0:87] not in L:
-		print "hey:"
-		#shutil.copyfile(pathR + '\\' + file,"C:\Users\Administrator\Desktop\cosData\\notL\\" + file)	
-		os.remove(pathR + '\\' + file)
-		print file
+	R.append(file[0:87])
+
+
+print len(L)
+print len(R)
+	# if file[0:87] not in L:
+	# 	print "hey:"
+	# 	#shutil.copyfile(pathR + '\\' + file,"C:\Users\Administrator\Desktop\cosData\\notL\\" + file)	
+	# 	os.remove(pathR + '\\' + file)
+	# 	print file
 #print L
 # for dir in dirs:
 # 	#with open("C:\Users\Administrator\Desktop\DirList.txt", 'a+') as s:
