eeed6a2ed83fcf5eda00eff8a168460347b91381
徐梓琅 <272251416@qq.com>
2017-11-28 10:17:44 +0800

    better

--- 10.文件批量重命名/copyFile.py
+++ 10.文件批量重命名/copyFile.py
@@ -6,16 +6,16 @@ path = "C:\Users\Administrator\Desktop\linxi"
 dirs = os.listdir(path)
 
 for dir in dirs:
-	#with open("C:\Users\Administrator\Desktop\DirList.txt", 'a+') as s:
-	#			s.write(str(n) + "    " + dir + '\n')
-	for file in os.listdir(path + '\\' + dir):
-	    
-	    if file.endswith('.jpg'):
-		    #newname = dir + '_Preprocessing_%08d_00.jpg' % n
-			shutil.copyfile(path + '\\' + dir + '\\' + file,"C:\Users\Administrator\Desktop\\linxiData\\rich_text\\" + file)
-			print file, 'Done'
-		elif file.endswith('.xml'):
-			newname = file[0:97] + '_label_20171127_00_00' + '.xml'
-	        #os.rename(path + '\\' + dir + '\\' + file, path + '\\' + dir + '\\' + newname)
-	        shutil.copyfile(path + '\\' + dir + '\\' + file,"C:\Users\Administrator\Desktop\\linxiData\\label_results\\" + newname)
-	        print file, 'Done'
+    #with open("C:\Users\Administrator\Desktop\DirList.txt", 'a+') as s:
+    #			s.write(str(n) + "    " + dir + '\n')
+    for file in os.listdir(path + '\\' + dir):
+
+        if file.endswith('.jpg'):
+            #newname = dir + '_Preprocessing_%08d_00.jpg' % n
+            shutil.copyfile(path + '\\' + dir + '\\' + file,"C:\Users\Administrator\Desktop\\linxiData\\rich_text\\" + file)
+            print file, 'Done'
+        elif file.endswith('.xml'):
+            newname = file[0:97] + '_label_20171127_00_00' + '.xml'
+            #os.rename(path + '\\' + dir + '\\' + file, path + '\\' + dir + '\\' + newname)
+            shutil.copyfile(path + '\\' + dir + '\\' + file,"C:\Users\Administrator\Desktop\\linxiData\\label_results\\" + newname)
+            print file, 'Done'
