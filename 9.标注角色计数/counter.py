f63d1514655dca6463635934271eb520e7a39838
徐梓琅 <272251416@qq.com>
2017-11-28 09:34:38 +0800

    better

--- 9.标注角色计数/counter.py
+++ 9.标注角色计数/counter.py
@@ -13,17 +13,18 @@ for Dir in dirs:
 	if not os.path.isdir(Dir):
 		files = os.listdir(path + '/' + Dir)
 		for file in files:
-			Path = 'file:///' + path + '/' + Dir + '/' + file
-			print Path
-			response = urllib.urlopen(Path)
-			html = response.read()
-			soup = BeautifulSoup(html, "xml")
-			names = soup.select("name")
+			if file.endswith(".xml"):
+				Path = 'file:///' + path + '/' + Dir + '/' + file
+				print Path
+				response = urllib.urlopen(Path)
+				html = response.read()
+				soup = BeautifulSoup(html, "xml")
+				names = soup.select("name")
 
-			for name in names:
-				if name.get_text().endswith('-head'):
-					nameSets.append(name.get_text())
-					print 'Done'
+				for name in names:
+					if name.get_text().endswith('-head'):
+						nameSets.append(name.get_text())
+						print 'Done'
 
 for n in nameSets:
     if name_counts.has_key(n):
