c5ac3db0ae10c587c7088c9a46e4cbcd86965434
徐梓琅 <272251416@qq.com>
2017-11-29 10:13:18 +0800

    better

--- 11.ffmpeg使用/test.py
+++ 11.ffmpeg使用/test.py
@@ -12,19 +12,19 @@ def cut_image(resource_path, output_file_path, output_record):
 
         index += 1
 
-        pre_file_name = 'mgdm_characterDetectionAI_Origin_Video_20171115_%08d_00' % index
+        pre_file_name = 'cartoon_characterDetectionAI_Origin_Video_20171129_0000060' + '%d' % index + '_00'
         output_path = os.path.join(output_file_path, pre_file_name)
-        with open("C:\Users\Administrator\Desktop\missions\huoying\Preprocessing\Preprocessing\Image\huoyingthename.txt", 'a+') as s:
+        with open("E:\nameList.txt", 'a+') as s:
             s.write('%s\t%s\n' % (file, pre_file_name))
 
         os.mkdir(output_path)
         os.chdir(output_path)
 
-        output_file_name = pre_file_name + '_Preprocessing_20171115_%08d_00.jpg'
+        output_file_name = pre_file_name + '_Preprocessing_20171129_%08d_00.jpg'
 
         subprocess.call(['ffmpeg', '-i', file_path, '-r', '1', output_file_name])  # 抽帧命令
  
-rp = 'E:\huoying-new2'
-op = 'E:\huoying-new2'
+rp = 'E:\pannideluluxiu'
+op = 'E:\pannideluluxiu'
 rc = 'nameList'
 cut_image(rp, op, rc)
