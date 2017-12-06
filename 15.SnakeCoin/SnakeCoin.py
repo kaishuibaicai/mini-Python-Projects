64c981d75dcb58c0c3e493ac9175d46977266568
徐梓琅 <272251416@qq.com>
2017-12-07 00:22:36 +0800

    add new snakecoin

---
+++ 15.SnakeCoin/SnakeCoin.py
@@ -0,0 +1,9 @@
+import hashlib as hasher
+
+class Block:
+	def __init__(self, index, timestamp, data, previous_hash):
+		self.index = index
+		self.timestamp = timestamp
+		self.data = data
+		self.previous_hash = previous_hash
+		self.hash = self.hash_block()
