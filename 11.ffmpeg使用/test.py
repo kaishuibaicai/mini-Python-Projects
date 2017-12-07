01184c6c0354c915d90b039017b968c1bdb355f2
徐梓琅 <272251416@qq.com>
2017-12-07 16:04:39 +0800

    add 16 prj delete the same picture

--- 11.ffmpeg使用/test.py
+++ 11.ffmpeg使用/test.py
@@ -4,7 +4,7 @@ import os, subprocess
 def cut_image(resource_path, output_file_path, output_record):
 
     files = os.listdir(resource_path)
-    index = 27
+    index = 10
     for file in files:
         file_path = os.path.join(resource_path, file)
         if os.path.isdir(file_path):
@@ -12,7 +12,7 @@ def cut_image(resource_path, output_file_path, output_record):
 
         index += 1
 
-        pre_file_name = 'cartoon_characterDetectionAI_Origin_Video_20171122_000000' + '%d' % index + '_00'
+        pre_file_name = 'cartoon_characterdetectionai_Origin_Video_20171129_000006' + '%02d' % index + '_00'
         output_path = os.path.join(output_file_path, pre_file_name)
         with open("E:\The Second\\nameList.txt", 'a+') as s:
             s.write('%s\t%s\n' % (file, pre_file_name))
@@ -20,11 +20,11 @@ def cut_image(resource_path, output_file_path, output_record):
         os.mkdir(output_path)
         os.chdir(output_path)
 
-        output_file_name = pre_file_name + '_Preprocessing_20171122_%08d_00.jpg'
+        output_file_name = pre_file_name + '_Preprocessing_20171129_%08d_00.jpg'
 
         subprocess.call(['ffmpeg', '-i', file_path, '-r', '1', output_file_name])  # 抽帧命令
  
-rp = 'E:\The Second\jinjidejure-buchong2\\new2'
-op = 'E:\The Second\jinjidejure-buchong2\\new2'
+rp = 'E:\The Second\pannideluluxiu\\new2'
+op = 'E:\The Second\pannideluluxiu\\new2'
 rc = 'nameList'
 cut_image(rp, op, rc)
--- 15.SnakeCoin/SnakeCoin.py
+++ 15.SnakeCoin/SnakeCoin.py
@@ -34,3 +34,4 @@ for i in range(0, num_of_block_to_add):
 	previous_block = block_to_add
 	print "Block #{} has been added to the blockchain!".format(block_to_add.index)
 	print "Hash: {}\n".format(block_to_add.hash)
+	
