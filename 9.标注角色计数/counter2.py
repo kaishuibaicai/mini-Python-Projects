e8fd5822198509505aaf0a0f24f603f2b3796214
徐梓琅 <272251416@qq.com>
2018-01-09 10:58:13 +0800

    sc

--- 9.标注角色计数/counter2.py
+++ 9.标注角色计数/counter2.py
@@ -22,7 +22,7 @@ def extract_character_name_list(xml_file):
 
 
 if __name__ == "__main__": 
-    path = 'C:\\Users\Administrator\Desktop\missions\哆啦A梦Preprocessing\新建文件夹'
+    path = 'E:\The Second\shenqibaobei'
 
     name_dict = dict()
     for folder in os.listdir(path):
