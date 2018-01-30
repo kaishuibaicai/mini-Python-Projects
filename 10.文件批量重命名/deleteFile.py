90addeda6539e288685f88448b4e7d9e9c40b201
徐梓琅 <272251416@qq.com>
2018-01-30 16:57:39 +0800

    better days

--- 10.文件批量重命名/deleteFile.py
+++ 10.文件批量重命名/deleteFile.py
@@ -2,7 +2,7 @@
 import os
 import shutil
 
-path = 'E:\The Second\美少女战士\\'
+path = 'E:\The Second\钢之炼金术师\cartoon_characterdetectionai_Origin_Video_20180122_00000040_00\\'
 #pathR = "C:\\Users\Administrator\Desktop\missions\哆啦A梦Preprocessing\新建文件夹\mgdm_characterDetectionAI_Origin_Video_20171108_00000013_00"
 #pathL = "C:\\Users\Administrator\Desktop\missions\哆啦A梦Preprocessing\新建文件夹\mgdm_characterDetectionAI_Origin_Video_20171108_00000013_00"
 for dir in os.listdir(path):
