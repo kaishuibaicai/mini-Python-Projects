bb2c99f53ff93ccfde74f9b1f15146ba68045318
徐梓琅 <272251416@qq.com>
2018-02-09 23:03:05 +0800

    better days

--- 9.标注角色计数/counter2.py
+++ 9.标注角色计数/counter2.py
@@ -22,7 +22,7 @@ def extract_character_name_list(xml_file):
 
 
 if __name__ == "__main__": 
-    path = 'H:\标注任务\\new'
+    path = 'H:\家庭教师\\task'
 
     name_dict = dict()
     for folder in os.listdir(path):
