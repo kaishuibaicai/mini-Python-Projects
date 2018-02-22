0f119c1c9cab63f69a7295ff3edba2f07b9760e4
徐梓琅 <272251416@qq.com>
2018-02-22 10:32:05 +0800

    18.1 done

--- 18.角色出场搜索/角色出场交集/run.py
+++ 18.角色出场搜索/角色出场交集/run.py
@@ -1,9 +1,17 @@
 from getlist import *
-
+import os
 
 def getInter(url):
 	I = []
 	for file in os.listdir(url):
 		L = getlist(url + '\\' + file)
 		if I:
-			I = []
+			I = [x for x in I if x in L]
+		else:
+			I = L
+
+	return I
+
+if __name__ == '__main__':
+	url = 'C:\\Users\\Administrator\\Desktop\\mini-Python-Projects\\18.角色出场搜索\\角色出场交集\\files'
+	print (getInter(url))
