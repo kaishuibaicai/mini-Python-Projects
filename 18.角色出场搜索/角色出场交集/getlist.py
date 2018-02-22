3709951bef7888105e417107500372793e019863
徐梓琅 <272251416@qq.com>
2018-02-22 09:47:13 +0800

    开工大吉，加油1

--- 18.角色出场搜索/角色出场交集/getlist.py
+++ 18.角色出场搜索/角色出场交集/getlist.py
@@ -10,8 +10,10 @@ def getlist(url):
 			line = line.lstrip().lstrip('0')[:4]	#这里的.lstrip()是去除字符串开头的空格，.rstrip()是去除字符串末尾的空格
 			Line = re.findall(r"\d+",line)
 			if Line:
-				print (Line[0])
+				#print (Line[0])
+				L.append(Line[0])
+	print (L)
 
 
 if __name__ == '__main__':
-	getlist('C:\\Users\\Administrator\\Desktop\\mini-Python-Projects\\18.角色出场搜索\\角色出场交集\\2.txt')
+	getlist('C:\\Users\\Administrator\\Desktop\\mini-Python-Projects\\18.角色出场搜索\\角色出场交集\\3.txt')
