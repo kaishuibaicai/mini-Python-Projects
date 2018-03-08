960dea2dbe724297e6c38a9e6ebdbe65cc7ad6eb
徐梓琅 <272251416@qq.com>
2018-03-08 09:45:08 +0800

    bts

--- 10.文件批量重命名/rename3.py
+++ 10.文件批量重命名/rename3.py
@@ -10,7 +10,7 @@ for file in os.listdir(path):
 	    
 	#if file.endswith('.xml'):    
 		#newname = 'mgdm_relevantCharacterAI_Origin_Image_20171116_' + '%08d' % n + '_00_Preprocessing_20171116_00.jpg'
-	newname = 'haichengtian%d.png' % n
+	newname = 'haichengtian_%d.png' % n
 	os.rename(path + '\\' + file, path  + '\\' + newname)
 
 	print (newname, 'Done')
