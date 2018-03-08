8b168b7b9e28e765bc304f1f324093a58e4cbf1b
徐梓琅 <272251416@qq.com>
2018-03-08 09:44:43 +0800

    btd

--- 10.文件批量重命名/rename3.py
+++ 10.文件批量重命名/rename3.py
@@ -1,18 +1,18 @@
 # -*- coding: utf-8 -*-
 import os
 
-path = "C:\\Users\Administrator\Desktop\missions\\toy\\toy\SuoLong\新建文件夹"
+path = "C:\\Users\\Administrator\\Desktop\\label\\new\\fzynt-海澄天\海澄天"
 
-n = 723
+n = 1
 
 	
 for file in os.listdir(path):
 	    
-	if file.endswith('.xml'):    
+	#if file.endswith('.xml'):    
 		#newname = 'mgdm_relevantCharacterAI_Origin_Image_20171116_' + '%08d' % n + '_00_Preprocessing_20171116_00.jpg'
-		newname = 'cartoon_toyCharacterAI_Origin_Image_20171225_%08d_00_Preprocessing_20171225_00.jpg' % n
-		os.rename(path + '\\' + file, path  + '\\' + newname)
-		    
-		print (newname, 'Done')
+	newname = 'haichengtian%d.png' % n
+	os.rename(path + '\\' + file, path  + '\\' + newname)
 
-		n = n + 1
+	print (newname, 'Done')
+
+	n = n + 1
