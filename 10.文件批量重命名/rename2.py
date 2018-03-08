458c123347d32ccfa649713c566b1bce4a425c2a
徐梓琅 <272251416@qq.com>
2018-03-08 21:40:05 +0800

    better days

--- 10.文件批量重命名/rename2.py
+++ 10.文件批量重命名/rename2.py
@@ -1,21 +1,21 @@
 # -*- coding: utf-8 -*-
 import os
 
-path = "C:\Users\Administrator\Desktop\label\\new\\fzynt-海澄天\海澄天"
+path = "H:\筛选图片"
 dirs = os.listdir(path)
-
+n = 1
 for dir in dirs:
-    n = 1
+    
     #with open("C:\Users\Administrator\Desktop\DirList.txt", 'a+') as s:
     #			s.write(str(n) + "    " + dir + '\n')
     for file in os.listdir(path + '\\' + dir):
 
-        if file.endswith('.jpg'):
-            newname = dir + '_Preprocessing_20171122_%08d_00.jpg' % n
-            #newname = file[0:87]  + '_label_20171117_00_00' + '.xml'
-            #newname = '%02d_' % n +  file
+        #if file.endswith('.jpg'):
+        newname = 'comic_20180308_%08d.jpg' % n
+        #newname = file[0:87]  + '_label_20171117_00_00' + '.xml'
+        #newname = '%02d_' % n +  file
 
-            os.rename(path + '\\' + dir + '\\' + file, path + '\\' + dir + '\\' + newname)
-            print (newname + '  Done')
+        os.rename(path + '\\' + dir + '\\' + file, path + '\\' + dir + '\\' + newname)
+        print (newname + '  Done')
 
         n = n + 1
--- 10.文件批量重命名/rename3.py
+++ 10.文件批量重命名/rename3.py
@@ -10,7 +10,7 @@ for file in os.listdir(path):
 	    
 	#if file.endswith('.xml'):    
 		#newname = 'mgdm_relevantCharacterAI_Origin_Image_20171116_' + '%08d' % n + '_00_Preprocessing_20171116_00.jpg'
-	newname = 'senyao_%d.png' % n
+	newname = 'comic_20180308_%08d.png' % n
 	os.rename(path + '\\' + file, path  + '\\' + newname)
 
 	print (newname, 'Done')
