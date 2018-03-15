19555663d7ce498364c027b191fb33a1fa97d861
徐梓琅 <272251416@qq.com>
2018-03-15 16:19:01 +0800

    better days

--- 20.任务派发/generList.py
+++ 20.任务派发/generList.py
@@ -1,13 +1,13 @@
 import os
-n = 1
-path = 'H:\标注管理\images\\0312'
+n = 5
+path = 'H:\标注管理\images\\0315\\qs'
 files = os.listdir(path)
 for i in range(len(files)):
-	if (i+1) % 20 == 1:
-		print ('\n\ntask_20180312_%02d\n\n["static/img/20180312/' % n + files[i]+ '",')
+	if (i+1) % 100 == 1:
+		print ('\n\ntask_20180315_%02d\n\n["static/img/20180312/' % n + files[i]+ '",')
 		n += 1
-	elif (i+1) % 20 == 0:
-		print ('"static/img/20180312/' + files[i]+ '"]\n\nhttp://112.5.162.246:180')
+	elif (i+1) % 100 == 0:
+		print ('"static/img/20180312/' + files[i]+ '"]')
 	else:
 		print ('"static/img/20180312/' + files[i]+ '",')
 
--- 20.任务派发/test2.py
+++ 20.任务派发/test2.py
@@ -1,10 +1,10 @@
 import os
 n = 1
-path = 'H:\标注管理\images\\0312'
+path = 'H:\标注管理\images\\0315\\新建文件夹2'
 files = os.listdir(path)
 for i in range(len(files)):
 	if (i+1) % 20 == 1:
-		print ('task_20180312_%02d' % n )
+		print ('task_20180315_%02d' % n )
 		n += 1
 	
 
