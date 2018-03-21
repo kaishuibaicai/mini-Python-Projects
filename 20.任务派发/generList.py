626d71092d60648b034a32d0388cdd49295fda56
徐梓琅 <272251416@qq.com>
2018-03-21 10:15:33 +0800

    sb

--- 20.任务派发/generList.py
+++ 20.任务派发/generList.py
@@ -1,15 +1,15 @@
 import os
-n = 5
-path = 'H:\标注管理\images\\0315\\qs'
+n = 1
+path = 'H:\标注管理\images\\0321\\pictures'
 files = os.listdir(path)
 for i in range(len(files)):
-	if (i+1) % 100 == 1:
-		print ('\n\ntask_20180315_%02d\n\n["static/img/20180312/' % n + files[i]+ '",')
+	if (i+1) % 98 == 1:
+		print ('\n\ntask_20180321_%02d\n\n["static/img/20180308/' % n + files[i]+ '",')
 		n += 1
-	elif (i+1) % 100 == 0:
-		print ('"static/img/20180312/' + files[i]+ '"]')
+	elif (i+1) % 98 == 0:
+		print ('"static/img/20180308/' + files[i]+ '"]')
 	else:
-		print ('"static/img/20180312/' + files[i]+ '",')
+		print ('"static/img/20180308/' + files[i]+ '",')
 
 #with open("H:\标注管理\\files.txt", 'a+') as s:
 #	s.write(files)
