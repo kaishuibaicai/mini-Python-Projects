ec19a57e104ae05ec1b7a6685ee46ff6f8ed3a5d
徐梓琅 <272251416@qq.com>
2018-03-23 18:44:18 +0800

    better days

--- 20.任务派发/task-trans.py
+++ 20.任务派发/task-trans.py
@@ -2,9 +2,9 @@ import os, shutil
 
 
 pathp = 'H:\标注管理\\images\\0315\\pictures'
-pathl = 'H:\标注管理\\images\\0315\\points2'
-pathr = 'H:\标注管理\\images\\0315\\results2'
-pathb = 'H:\标注管理\\images\\0315\\boxes2'
+pathl = 'H:\标注管理\\images\\0315\\points3'
+pathr = 'H:\标注管理\\images\\0315\\results3'
+pathb = 'H:\标注管理\\images\\0315\\boxes3'
 
 
 task_done = open('H:\标注管理\\images\\0315\\task-done.txt','r', encoding='UTF-8')
