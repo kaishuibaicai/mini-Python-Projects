775da9edd9ce7608da996742b3b6d1efe194109d
徐梓琅 <272251416@qq.com>
2018-03-24 13:50:45 +0800

    better dayas

--- 20.任务派发/generList.py
+++ 20.任务派发/generList.py
@@ -1,16 +1,16 @@
 import os, random
 n = 1
-path = 'H:\标注管理\images\\0321\\pictures'
+path = 'H:\标注管理\\images\\0324\pictures'
 files = os.listdir(path)
 random.shuffle(files) 
 for i in range(len(files)):
-	if (i+1) % 57 == 1:
-		print ('\n\ntask_20180321_%02d\n\n["static/img/20180308/' % n + files[i]+ '",')
+	if (i+1) % 60 == 1:
+		print ('\n\ntask_20180324_%02d\n\n["static/img/20180307/' % n + files[i]+ '",')
 		n += 1
-	elif (i+1) % 57 == 0:
-		print ('"static/img/20180308/' + files[i]+ '"]')
+	elif (i+1) % 60 == 0:
+		print ('"static/img/20180307/' + files[i]+ '"]')
 	else:
-		print ('"static/img/20180308/' + files[i]+ '",')
+		print ('"static/img/20180307/' + files[i]+ '",')
 
 #with open("H:\标注管理\\files.txt", 'a+') as s:
 #	s.write(files)
