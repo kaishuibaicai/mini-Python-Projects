66f586681542d497c050a43e7e6dcb7bd8e4c3bb
徐梓琅 <272251416@qq.com>
2018-03-30 00:01:36 +0800

    add DjangoL

---
+++ 22.ID查重/test.py
@@ -0,0 +1,23 @@
+# 导入模块
+from wxpy import *
+# 初始化机器人，扫码登陆
+bot = Bot()
+
+
+# 打印来自其他好友、群聊和公众号的消息
+@bot.register()
+def print_others(msg):
+    print(msg)
+
+# 回复 my_friend 的消息 (优先匹配后注册的函数!)
+@bot.register(my_friend)
+def reply_my_friend(msg):
+    return 'received: {} ({})'.format(msg.text, msg.type)
+
+# 自动接受新的好友请求
+@bot.register(msg_types=FRIENDS)
+def auto_accept_friends(msg):
+    # 接受好友请求
+    new_friend = msg.card.accept()
+    # 向新的好友发送消息
+    new_friend.send('哈哈，我自动接受了你的好友请求')
