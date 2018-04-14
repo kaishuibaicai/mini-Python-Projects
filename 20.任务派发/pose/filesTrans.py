a81a637a9726b8ff391ca389b4b0e526652f06ea
徐梓琅 <272251416@qq.com>
2018-04-15 00:09:31 +0800

    111

--- 20.任务派发/pose/filesTrans.py
+++ 20.任务派发/pose/filesTrans.py
@@ -1,8 +1,8 @@
 import os, shutil
-
-basePath = 'H:\标注管理\\images\\0411舞蹈动作\图片收集'
-picPath = 'H:\标注管理\\images\\0327舞蹈动作\pictures'
-taskLPath = 'H:\标注管理\\images\\0327舞蹈动作\任务分配\\taskList\\'
+baseP = 'H:\标注管理\\images\\0411舞蹈动作'
+basePath = os.path.join(baseP, '图片收集')
+picPath = os.path.join(baseP, 'pictures')
+taskLPath = os.path.join(baseP, 'taskList\\')
 
 c = 1
 for people in os.listdir(basePath):
@@ -12,17 +12,17 @@ for people in os.listdir(basePath):
 	if os.path.exists(taskFile):
 		os.remove(taskFile)
 	tf = open(taskFile, 'a+')
-	tf.write('task_20180330_' + people[0:2] + '\n')
+	tf.write('task_20180411_' + people[0:2] + '\n')
 	tf.write('[')
 	for dirs in peopleList:
 
 		
 		picList = os.listdir(os.path.join(basePath, people, dirs))
 		for file in picList:
-			newname = 'pose_20180330_' + people[0:2] + '%06d.jpg' % c
-			#shutil.copyfile(os.path.join(basePath, people, dirs, file), os.path.join(picPath, newname))
-			tf.write('"static/img/20180330/%s",\n' % newname)
-			#print (newname, 'Done')
+			newname = 'pose_20180411_' + people[0:2] + '%06d.jpg' % c
+			shutil.copyfile(os.path.join(basePath, people, dirs, file), os.path.join(picPath, newname))
+			tf.write('"static/img/20180411/%s",\n' % newname)
+			print (newname, 'Done')
 			c += 1
 	
 					
