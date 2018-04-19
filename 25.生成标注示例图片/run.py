d2ec9a1397be1629e587ea5b68d81e453d5ab3d3
徐梓琅 <272251416@qq.com>
2018-04-19 11:41:02 +0800

    bts

---
+++ 25.生成标注示例图片/run.py
@@ -0,0 +1,27 @@
+from xml.etree.ElementTree import parse
+from PIL import Image, ImageDraw
+import os
+
+filepath = 'H:\标注管理\\images\标注方法介绍\\5\\kenan'
+savepPath = 'H:\标注管理\\images\标注方法介绍\\5\\角色标注示例'
+
+for i in os.listdir(filepath):
+	if i.endswith('xml'):
+		im = Image.open(os.path.join(filepath, i[:-3] + 'jpg'))
+		draw = ImageDraw.Draw(im)
+		xml_tree = parse(os.path.join(filepath, i))
+		root = xml_tree.getroot()
+		for label in root.findall('object'):
+			label_name = label.find('name').text
+			print (label_name)
+			p = []
+			for n in label.find('bndbox'):
+				p.append(int(n.text))
+
+			xmin, ymin, xmax, ymax = p
+			print (xmin, ymin, xmax, ymax)
+
+			draw.rectangle((xmin, ymin, xmax, ymax), outline='red')
+			draw.text()
+		im.save(os.path.join(savepPath, i[:-3] + 'jpg'))
+			
