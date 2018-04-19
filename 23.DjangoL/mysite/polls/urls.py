1a45ae5893e821588f360b91fad0d3ee621bf543
徐梓琅 <272251416@qq.com>
2018-04-19 09:08:24 +0800

    betterdays

--- 23.DjangoL/mysite/polls/urls.py
+++ 23.DjangoL/mysite/polls/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
 	path('', views.index, name='index'),
+	path('<int:question_id>/', views.detail, nmae='detail'),
+	path('<int:question_id>/results/', views.resultsm, name='results'),
+	path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
